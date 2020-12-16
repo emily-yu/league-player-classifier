92f7c59adf7146bd91503d9741836ac4695ca67a
Emily Yu <eyudeveloper@gmail.com>
2020-12-16 01:18:57 -0800

    oops json format

--- ritoapi_cache/game_samples.py
+++ ritoapi_cache/game_samples.py
@@ -6,4 +6,5 @@ with open('ritoapi_cache/match/3706547077.json', 'r') as f2:
     matchdata = f2.read()
     print(matchdata)
 match1 = json.loads(matchdata)
+match1 = json.dumps(match1, indent = 2)
 print(match1)
