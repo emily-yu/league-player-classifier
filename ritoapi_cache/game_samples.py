fe5ff7c7d51be78768019a8e09c96f6d25f73e43
Emily Yu <eyudeveloper@gmail.com>
2020-12-16 01:34:23 -0800

    3 matches for summoner1 mentalbooom

--- ritoapi_cache/game_samples.py
+++ ritoapi_cache/game_samples.py
@@ -1,10 +1,15 @@
 import json
+import os
+# from https://na1.api.riotgames.com/lol/match/v4/matches/3706044908
 
-# load singular match from match folder
+# load all matches from match folder
 matchdata = None
-with open('ritoapi_cache/match/3706547077.json', 'r') as f2:
-    matchdata = f2.read()
-    print(matchdata)
+count = 0
+for entry in os.scandir('ritoapi_cache/match'):
+    with open(entry, 'r') as f2:
+        matchdata = f2.read()
+        # print(matchdata)
+        print(count)
 match1 = json.loads(matchdata)
 match1 = json.dumps(match1, indent = 2)
-print(match1)
+# print(match1)
