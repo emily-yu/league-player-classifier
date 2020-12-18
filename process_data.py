c4f48e6bea8e729f577609c24c33d796784793ab
Emily Yu <eyudeveloper@gmail.com>
2020-12-18 05:00:40 -0800

    dump out old stuff

--- process_data.py
+++ process_data.py
@@ -1,158 +1,25 @@
 
 import pandas as pd
-# df = pd.read_csv('write.csv')
-# print(df)
+import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 
-
-
-
-
-
-
-import pickle
-# with open('data/match_data_version1.pickle', 'rb') as fid:
-#      data3 = pickle.load(fid)
-#      print('Data After  Read :', data3)
-
-# data3
-
-print("helo")
 N_CLUSTERS = 5
 
-# import xlrd
-# !pip install xlrd
-import pandas as pd
+# df = pd.read_csv('oldwriteto.csv')
+df = pd.read_csv('write.csv')
 
-# df = pd.read_excel('league_pro_matches_data/2019-spring-match-data-OraclesElixir-2019-05-21.xlsx')
-df = pd.read_csv('oldwriteto.csv')
-# df
-
-# analyze per roles, since each role is played differently / has different goals and purpose within game
-# each player should generally stick to their own role on their own team, so analyze per PRO PLAYER within ROLE
-# top = df.loc[df['position'] == 'Top']
-# print("TOP DF")
-# top
-
-# mid = df[df['position'] == 'Middle']
-# print("MID DF")
-# print(mid)
-
-# sup = df[df['position'] == 'Support']
-# print("SUPPORT DF")
-# print(sup)
-
-# adc = df[df['position'] == 'ADC']
-# print(adc)
-
-# jg = df[df['position'] == 'Jungle']
-# print(jg)
-
-'''
-random notes
-- for number comparisons, get average of all PRO data and see how PLAYER data compares
-
-qualitative cols
-- champion pool (get over ALL games)
-- ban pool (ban1, ban2, ban3, ban4, ban5)
-    pick top 5 most common bans over ALL games
-
-quantitative cols
-- average win rate (result)
-    if average wr > 0.5, consider as winning player
-- kda ratio comparison (in terms of relative team contribution)
-- 
-
-cols: 
-- doubles, triples, quadras, pentas
-- fb (first blood)
-    - fbassist (first blood assist)
-    - fbvictim (first blood victim)
-    - fbtime (first blood time)
-- kpm (kills per min)
-    - okpm
-    - ckpm (creep kills per min)
-- fd (first death)
-    - fdtime (first death time)
-- for objective control...
-    - teamdragkill
-    - oppdragkill
-    - elementals
-    - oppelemental
-    - firedrake
-    - waterdrake
-    - earthdrake
-    - airdrakes
-    - elders
-    - oppelders
-    - herald
-    - heraldtime
-- ft (first tower)
-    - fttime
-- firstmid
-- firstmidouter
-- firsttothreetowers
-- teamtowerkills
-- opptowerkills
-- fbaron
-- fbarontime
-- teambaronkills
-- oppbaronkills
-- dmgtochamps
-- dmgtochampsperminute
-- dmgshare
-- earnedgoldshare
-- wards
-- wpm
-- wardshare
-- wardkills
-- wcpm
-- visionwards
-- visionwardbuys
-- visiblewardclearrate
-- invisiblewardclearrate
-- totalgold
-- earnedgpm
-- goldspent
-- gspd
-- minionkills
-- monsterkills
-- monsterkillsownjungle
-- monsterkillsenemyjungle
-- cspm
-- goldat10
-- oppgoldat10
-- gdat10
-- goldat15
-- oppgoldat15
-- gdat15
-- xpat10
-- oppxpat10
-- xpdat10
-- csat10
-- oppcsat10
-- csdat10
-- csat15
-- oppcsat15
-- csdat15
-
-for extension: average game length (use for game comparisons)
-    to find which pro game would be most similar
-    to try and pick a winning team, pick similar normal players to the PRO non-aggregate row values
-        [tbd]
-'''
 # ======= to consider to have some irrelevant graphs on pro players ========
 # for players that get dropped from roster: visualize how a player changes over time, what does their performance look like until they get dropped from the roster?
 # does side affect how the player acts?
 
-# remove irrelevant columns
-# df.drop(columns=['gameid', 'url', 'league', 'split', 'date', 'week', 'patchno', 'side', 'playerid', 'game'], axis=1, inplace=True)
-
-# Commented out IPython magic to ensure Python compatibility.
-# KEY STEPS (number one priority)
-
 ##### > select based on ROLE [after WIP], to classify players based on ROLE (since TOP will have diff stats from SUPP, etc.)**
+
 ##### > average out data of each pro player for kmeans
-# avgs = df.groupby('player').mean()
 avgs = df.groupby('summonerName').mean()
 print(avgs.columns)
 print("avgs")
@@ -160,119 +27,7 @@ print(avgs)
 avgs = avgs.fillna(0)
 
 
-##### > create kmenas clusters for QUANTIATIVE VALUES [TODO] 
-# ...fill any values with averages of all players to make sure no missing values https://www.datacamp.com/community/tutorials/k-means-clustering-python
-# ...all numerical values because good for quantative values
-import pandas as pd
-import numpy as np
-from sklearn.cluster import KMeans
-# from sklearn.preprocessing import LabelEncoder
-# from sklearn.preprocessing import MinMaxScaler
-# !pip3 install seaborn
-import seaborn as sns
-import matplotlib.pyplot as plt
-# %matplotlib inline
-
-# ref https://datascience.stackexchange.com/questions/16700/confused-about-how-to-apply-kmeans-on-my-a-dataset-with-features-extracted
-# kmeans = KMeans(n_clusters=2) # number of clusters
-# kmeans.fit(X)
-
-# KMeans(algorithm='auto', copy_x=True, init='k-means++', max_iter=300,
-#     n_clusters=2, n_init=10, n_jobs=1, precompute_distances='auto',
-#     random_state=None, tol=0.0001, verbose=0)
-
-# # X = np.array(train.drop(['Survived'], 1).astype(float))
-# X = np.array(avgs)
-# correct = 0
-# for i in range(len(X)):
-#     predict_me = np.array(X[i].astype(float))
-#     predict_me = predict_me.reshape(-1, len(predict_me))
-#     prediction = kmeans.predict(predict_me)
-#     if prediction[0] == y[i]:
-#         correct += 1
-
-# print(correct/len(X))
-'''
-#Cluster the data
-kmeans = KMeans(n_clusters=5, random_state=0).fit(avgs)
-labels = kmeans.labels_
-
-centroids = kmeans.cluster_centers_
-print("cnetrodis: ")
-print(centroids)
-
-#Glue back to originaal data
-avgs['clusters'] = labels
-
-#Add the column into our list
-# clmns.extend(['clusters'])
-
-#Lets analyze the clusters
-print(avgs[:].groupby(['clusters']).mean())
-#Scatter plot of Wattage and Duration
-sns.lmplot('k', 'd', 
-           data=avgs, 
-           fit_reg=False, 
-           hue="clusters",  
-           scatter_kws={"marker": "D", 
-                        "s": 100})
-plt.title('Clusters Kills vs Deahts')
-plt.xlabel('Kills')
-plt.ylabel('Deaths')'''
-
-# #3 Using the elbow method to find out the optimal number of #clusters. 
-# #KMeans class from the sklearn library.
-# # from sklearn.cluster import KMeans
-# X = avgs
-# wcss=[]
-# #this loop will fit the k-means algorithm to our data and 
-# #second we will compute the within cluster sum of squares and #appended to our wcss list.
-# for i in range(1,len(avgs.columns)): 
-#      kmeans = KMeans(n_clusters=i, init ='k-means++', max_iter=300,  n_init=len(avgs.columns),random_state=0 )
-# #i above is between 1-10 numbers. init parameter is the random #initialization method  
-# #we select kmeans++ method. max_iter parameter the maximum number of iterations there can be to 
-# #find the final clusters when the K-meands algorithm is running. we #enter the default value of 300
-# #the next parameter is n_init which is the number of times the #K_means algorithm will be run with
-# #different initial centroid.
-# kmeans.fit(X)
-# #kmeans algorithm fits to the X dataset
-# print("................")
-# print(kmeans)
-# print(kmeans.inertia_)
-# wcss.append(kmeans.inertia_)
-# #kmeans inertia_ attribute is:  Sum of squared distances of samples #to their closest cluster center.
-# #4.Plot the elbow graph
-# plt.plot(range(1,len(avgs.columns)),wcss)
-# plt.title('The Elbow Method Graph')
-# plt.xlabel('Number of clusters')
-# plt.ylabel('WCSS')
-# plt.show() # ['stopped here'] https://medium.com/pursuitnotes/k-means-clustering-model-in-6-steps-with-python-35b532cfa8ad <<<<<<<<<<<<<<<<<<<<
-
-# for QUALITATIVE VALUES [bans, champs]: 
-# https://stackoverflow.com/questions/14720324/compute-the-similarity-between-two-lists
-# print("players: ", df.player.unique()) # players to pull qual data for
-# print("number of players: ", len(df.player.unique())) # players to pull qual data for
-
-# print("QUAL DATA")
-# champion_pool = bwipo['champion'].value_counts()
-# print('champion pool :')
-# print(champion_pool)
-
-# roles = bwipo['position'].value_counts() # for funs
-# print('roles :')
-# print(champion_pool)
-
-# ban_pool = bwipo[['ban1', 'ban2', 'ban3', 'ban4', 'ban5']] # not player specific, but player probably has some input; can use to calculate champion ban frequency against champions in player if bored, [irrelevant]
-# print(ban_pool)
-
-##### > average between quantatative and qualitative values to consider all elements in data
-# ( for game analysis, only use bans, as player's champ they played that game / want to play that game is picked by the player. alternatively, can do reverse analysis picks for what champion to play )
-
-# k means implementation from: https://github.com/OpenClassrooms-Student-Center/Multivariate-Exploratory-Analysis/blob/master/3a.%20K-Means%20Clustering.ipynb
-
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import StandardScaler
-
+##### create kmenas clusters for QUANTIATIVE VALUES
 def display_factorial_planes(X_projected, n_comp, pca, axis_ranks, labels=None, alpha=1, illustrative_var=None):
     '''Display a scatter plot on a factorial plane, one for each factorial plane'''
 
