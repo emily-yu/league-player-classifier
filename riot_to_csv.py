43ec8bdfa6337bc0ce5fe066013f8d02aa554d28
Emily Yu <eyudeveloper@gmail.com>
2020-12-18 02:53:25 -0800

    rest of qual stats

--- riot_to_csv.py
+++ riot_to_csv.py
@@ -87,13 +87,48 @@ def flatten(data, participant):
         return result
 
     stats = participant["stats"]
-    props = ["item0", 
+    props = [
+             "summonerName"
+             "championId",
+             "spell1Id", 
+             "spell2Id",
+             "item0", 
              "item1", 
              "item2", 
              "item3", 
              "item4", 
              "item5", 
              "item6",
+             "perk0",
+             "perk0Var1", 
+             "perk0Var2",
+             "perk0Var3", 
+             "perk1",
+             "perk1Var1", 
+             "perk1Var2",
+             "perk1Var3",    
+             "perk2",
+             "perk2Var1", 
+             "perk2Var2",
+             "perk2Var3",    
+             "perk3",
+             "perk3Var1", 
+             "perk3Var2",
+             "perk3Var3",      
+             "perk4",
+             "perk4Var1", 
+             "perk4Var2",
+             "perk4Var3",      
+             "perk5",
+             "perk5Var1", 
+             "perk5Var2",
+             "perk5Var3",
+             "perkPrimaryStyle",
+             "perkSubStyle",
+             "statPerk1",
+             "statPerk2",
+             "role"
+             "lane"              
              ]
     qualt_stats = select_properties(props, stats)
 
