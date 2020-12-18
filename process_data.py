803325bee5c3971ef1dedbab68fc0140e8d3ccbe
Emily Yu <eyudeveloper@gmail.com>
2020-12-18 13:18:00 -0800

    notes

--- process_data.py
+++ process_data.py
@@ -28,8 +28,23 @@ for i, c in enumerate(clusters):
     print(clusters[i]) # <<< clusters to use
     print(clusters[i].columns)
 
+    # merge cluster on qualitative values 
+
 # ======= to consider to have some irrelevant graphs on pro players ========
 # for players that get dropped from roster: visualize how a player changes over time, what does their performance look like until they get dropped from the roster?
 # does side affect how the player acts?
 
 ##### > select based on ROLE [after WIP], to classify players based on ROLE (since TOP will have diff stats from SUPP, etc.)**
+
+
+
+### reccomendation system useing kmeans -> collaborative filtering
+# find cluster user is in
+def cluster_with_user(user):
+    pass
+
+# find similar in cluster
+def reccommendation_for_similar_user(cluster, user):
+    pass
+
+## print top 5 recommendations
