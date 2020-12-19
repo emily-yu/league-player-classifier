45272980b0956544a13a65d86ae6a7d2e3c31c75
emily-yu <eyudeveloper@gmail.com>
2020-12-19 01:18:29 -0800

    f(cluster with user)

--- process_data.py
+++ process_data.py
@@ -26,7 +26,6 @@ print("............................................back to main file............
 for i, c in enumerate(clusters):
     print('cluster ', i, 'number of players in cluster: ', len(clusters[i]))
 
-
 # ======= to consider to have some irrelevant graphs on pro players ========
 # for players that get dropped from roster: visualize how a player changes over time, what does their performance look like until they get dropped from the roster?
 # does side affect how the player acts?
@@ -37,8 +36,14 @@ for i, c in enumerate(clusters):
 
 ### reccomendation system useing kmeans -> collaborative filtering
 # find cluster user is in
-def cluster_with_user(user):
-    pass
+def cluster_with_user(user, clusters):
+    for ind, cluster in enumerate(clusters):
+        names = cluster['summonerName'].tolist()
+        if user in names:
+            return (ind, cluster)
+    return None
+
+print('cluster ', cluster_with_user('C9 VULCAN', clusters)[0]) # example
 
 # find similar in cluster
 def reccommendation_for_similar_user(cluster, user):
