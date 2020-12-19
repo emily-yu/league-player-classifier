ae8edd6fae9dfd3546843776c3447eacca215be1
emily-yu <eyudeveloper@gmail.com>
2020-12-19 05:51:41 -0800

    good nuff maN

--- riot_to_csv_pro.py
+++ riot_to_csv_pro.py
@@ -35,7 +35,10 @@ def get_matchlist(accountId):
 
 # automate pulling match data from matchURL
 import time
-def get_match_data(match_ids):
+def get_match_data(matchlist):
+    if matchlist is None:
+        return []
+        
     result = []
     for region, match_id in matchlist:
         request_url = 'https://' + region + '.api.riotgames.com/lol/match/v4/matches/' + str(match_id) + '?api_key=' + api_key
@@ -63,7 +66,10 @@ def compile_participant_data(request, summonerName):
     participant_detail = request["participants"]
     
     # participant_mapping for SELF ONLY
-    mapping = next(item for item in participant_mapping if item["player"]["summonerName"] == summonerName)
+    mapping = next((item for item in participant_mapping if item["player"]["summonerName"] == summonerName), None)
+    if mapping is None:
+        return None
+
     id = mapping["participantId"]
     p_data = next(item for item in participant_detail if item["participantId"] == id)
     p_data["summonerName"] = mapping["player"]["summonerName"]
@@ -224,6 +230,178 @@ def pj(inp):
 
 #####################################################################################################################################################################################
 
+def write_summoner_to_csv(summonerName, has_headers):
+    accountId = get_player_matches(summonerName)
+    # print(accountId)
+
+    matchlist = get_matchlist(accountId)
+    # print(matchlist)
+
+    data = get_match_data(matchlist)
+    # print(data)
+
+    writeable = []
+    writeableq = [] # qualitative values (not used for clustering)
+
+    for first_match in data:
+        first_match = first_match.json()
+        match_data = compile_participant_data(first_match, summonerName)
+        if match_data is None:
+            continue
+
+        flattened = flatten(first_match, match_data)
+
+        # write quant data
+        base = {
+            "summonerName": flattened[0]["summonerName"]
+        } 
+        base.update(flattened[1])
+        writeable.append(base)
+
+        # write qual data
+        base = {
+            "summonerName": flattened[0]["summonerName"]
+        }
+        base.update(flattened[0])
+        writeableq.append(base)
+
+    # print(writeable)
+    # print(writeableq)
+
+    # put into csv
+    import csv
+    def to_csv(row, filepath):
+        with open(filepath, 'a') as f:  # Just use 'w' mode in 3.x
+            w = csv.DictWriter(f, row.keys())
+            w.writerow(row)
+
+    if not has_headers:
+        ### write header
+        with open('write_quant_pro.csv', 'w') as f:  # Just use 'w' mode in 3.x
+            w = csv.DictWriter(f, writeable[0].keys())
+            w.writeheader()
+        with open('write_qual_pro.csv', 'w') as f:  # Just use 'w' mode in 3.x
+            w = csv.DictWriter(f, writeableq[0].keys())
+            w.writeheader()
+
+        ### write data
+        for match in writeable:
+            to_csv(match, 'write_quant_pro.csv')
+
+
+
+
+    ### write qualitative data
+
+    # conversions for qualitative data (ids in riot system) to filtering
+    def process_champion_data(id):
+        x = requests.get('https://cdn.communitydragon.org/10.25.1/champion/' + str(id) + '/data')
+        return x.json()["name"]
+
+    # championName = process_champion_data(83)
+    # print(championName)
+
+    def process_item_data(item_number):
+        f = open('cdragon_en_US/item.json') 
+        items = json.load(f)
+
+        # catch nonexistent lul
+        if str(item_number) not in items["data"]: 
+            return "NaN"
+
+        return items["data"][str(item_number)]["name"]
+
+    # itemName = process_item_data(3153)
+    # print(itemName)
+
+    def process_perk_data(perkData): # [perk1, perk1Var1, perk1Var2, perk1Var3]
+        # edge case
+        if perkData == 0:
+            return ""
+
+        f = open('cdragon_en_US/runesReforged.json') 
+        runes = json.load(f)
+        # flatten
+        flattened = []
+        # print("flatten")
+        # print(json.dumps(runes, indent=2))
+        for i in range(len(runes)):
+            for elem in runes[i]["slots"]: 
+                flattened += elem["runes"]
+
+        # print(json.dumps(flattened, indent=2))
+        result = next((x for x in flattened if x["id"] == perkData), None)
+
+        # catch nonexistent lul
+        if result is None:
+            return 'NaN'
+
+        return result["key"]
+
+    # perkName = process_perk_data(8237)
+    # print(perkName)
+
+    def process_spell_data(spell_id):
+        f = open('cdragon_en_US/summoner.json') 
+        summoner_metadata = json.load(f)
+        # flatten
+        flattened = []
+        # print("flatten")
+        # print(json.dumps(summoner_metadata["data"], indent=2))
+        result = next((summoner_metadata["data"][i] for i in summoner_metadata["data"] if summoner_metadata["data"][i]["key"] == str(spell_id)), None)
+        # print('result', result)
+
+        if result is None:
+            return 'NaN'
+        
+        return result["name"]
+
+    # spellName = process_spell_data(12)
+    # print(spellName)
+
+    # replace item names
+    for match in writeableq:
+        props = ['item0', 'item1', 'item2', 'item3', 'item4', 'item5', 'item6']
+        for key in range(len(props)):
+            item_id = props[key]
+            match[item_id] = process_item_data(match[item_id])
+        
+        props = [
+            'perk0', 'perk0Var1', 'perk0Var2', 'perk0Var3', 
+            'perk1', 'perk1Var1', 'perk1Var2', 'perk1Var3', 
+            'perk2', 'perk2Var1', 'perk2Var2', 'perk2Var3', 
+            'perk3', 'perk3Var1', 'perk3Var2', 'perk3Var3',
+            'perk4', 'perk4Var1', 'perk4Var2', 'perk4Var3',
+            'perk5', 'perk5Var1', 'perk5Var2', 'perk5Var3',
+            'perkPrimaryStyle',
+            'perkSubStyle',
+            'statPerk1', 'statPerk2'
+            ]
+        for key in range(len(props)):
+            perk_id = props[key]
+            match[perk_id] = process_perk_data(match[perk_id])
+
+        props = [
+            'championId'
+        ]
+        for key in range(len(props)):
+            champ_id = props[key]
+            match[champ_id] = process_champion_data(match[champ_id])
+
+        props = [
+            'spell1Id',
+            'spell2Id'
+        ]
+        for key in range(len(props)):
+            spell_id = props[key]
+            match[spell_id] = process_spell_data(match[spell_id])
+
+    # perform write operation
+    for match in writeableq:
+        to_csv(match, 'write_qual_pro.csv')
+
+#####################################################################################################################################################################################
+
 df = pd.read_csv('league_pro_matches_data/2019-spring-match.csv')
 players_to_follow = df['player'].unique()
 
@@ -235,168 +413,9 @@ server_mappings = {
 # create script to automate riot api sample requests and fill in csv
 api_key = input("API KEY: ")
 
-summonerName = 'Bwipo'
-accountId = get_player_matches(summonerName)
-# print(accountId)
+# summonerName = 'Bwipo'
+has_headers = False
 
-matchlist = get_matchlist(accountId)
-# print(matchlist)
-
-data = get_match_data(matchlist)
-# print(data)
-
-writeable = []
-writeableq = [] # qualitative values (not used for clustering)
-
-for first_match in data:
-    first_match = first_match.json()
-    match_data = compile_participant_data(first_match, summonerName)
-    flattened = flatten(first_match, match_data)
-
-    # write quant data
-    base = {
-        "summonerName": flattened[0]["summonerName"]
-    } 
-    base.update(flattened[1])
-    writeable.append(base)
-
-    # write qual data
-    base = {
-        "summonerName": flattened[0]["summonerName"]
-    }
-    base.update(flattened[0])
-    writeableq.append(base)
-
-# print(writeable)
-# print(writeableq)
-
-# put into csv
-import csv
-def to_csv(row, filepath):
-    with open(filepath, 'a') as f:  # Just use 'w' mode in 3.x
-        w = csv.DictWriter(f, row.keys())
-        w.writerow(row)
-
-### write header
-with open('write_quant_pro.csv', 'w') as f:  # Just use 'w' mode in 3.x
-    w = csv.DictWriter(f, writeable[0].keys())
-    w.writeheader()
-with open('write_qual_pro.csv', 'w') as f:  # Just use 'w' mode in 3.x
-    w = csv.DictWriter(f, writeableq[0].keys())
-    w.writeheader()
-
-### write data
-for match in writeable:
-    to_csv(match, 'write_quant_pro.csv')
-
-
-
-
-### write qualitative data
-
-# conversions for qualitative data (ids in riot system) to filtering
-def process_champion_data(id):
-    x = requests.get('https://cdn.communitydragon.org/10.25.1/champion/' + str(id) + '/data')
-    return x.json()["name"]
-
-# championName = process_champion_data(83)
-# print(championName)
-
-def process_item_data(item_number):
-    f = open('cdragon_en_US/item.json') 
-    items = json.load(f)
-
-    # catch nonexistent lul
-    if str(item_number) not in items["data"]: 
-        return "NaN"
-
-    return items["data"][str(item_number)]["name"]
-
-# itemName = process_item_data(3153)
-# print(itemName)
-
-def process_perk_data(perkData): # [perk1, perk1Var1, perk1Var2, perk1Var3]
-    # edge case
-    if perkData == 0:
-        return ""
-
-    f = open('cdragon_en_US/runesReforged.json') 
-    runes = json.load(f)
-    # flatten
-    flattened = []
-    # print("flatten")
-    # print(json.dumps(runes, indent=2))
-    for i in range(len(runes)):
-        for elem in runes[i]["slots"]: 
-            flattened += elem["runes"]
-
-    # print(json.dumps(flattened, indent=2))
-    result = next((x for x in flattened if x["id"] == perkData), None)
-
-    # catch nonexistent lul
-    if result is None:
-        return 'NaN'
-
-    return result["key"]
-
-# perkName = process_perk_data(8237)
-# print(perkName)
-
-def process_spell_data(spell_id):
-    f = open('cdragon_en_US/summoner.json') 
-    summoner_metadata = json.load(f)
-    # flatten
-    flattened = []
-    # print("flatten")
-    # print(json.dumps(summoner_metadata["data"], indent=2))
-    result = next((summoner_metadata["data"][i] for i in summoner_metadata["data"] if summoner_metadata["data"][i]["key"] == str(spell_id)), None)
-    # print('result', result)
-
-    if result is None:
-        return 'NaN'
-    
-    return result["name"]
-
-# spellName = process_spell_data(12)
-# print(spellName)
-
-# replace item names
-for match in writeableq:
-    props = ['item0', 'item1', 'item2', 'item3', 'item4', 'item5', 'item6']
-    for key in range(len(props)):
-        item_id = props[key]
-        match[item_id] = process_item_data(match[item_id])
-    
-    props = [
-        'perk0', 'perk0Var1', 'perk0Var2', 'perk0Var3', 
-        'perk1', 'perk1Var1', 'perk1Var2', 'perk1Var3', 
-        'perk2', 'perk2Var1', 'perk2Var2', 'perk2Var3', 
-        'perk3', 'perk3Var1', 'perk3Var2', 'perk3Var3',
-        'perk4', 'perk4Var1', 'perk4Var2', 'perk4Var3',
-        'perk5', 'perk5Var1', 'perk5Var2', 'perk5Var3',
-        'perkPrimaryStyle',
-        'perkSubStyle',
-        'statPerk1', 'statPerk2'
-        ]
-    for key in range(len(props)):
-        perk_id = props[key]
-        match[perk_id] = process_perk_data(match[perk_id])
-
-    props = [
-        'championId'
-    ]
-    for key in range(len(props)):
-        champ_id = props[key]
-        match[champ_id] = process_champion_data(match[champ_id])
-
-    props = [
-        'spell1Id',
-        'spell2Id'
-    ]
-    for key in range(len(props)):
-        spell_id = props[key]
-        match[spell_id] = process_spell_data(match[spell_id])
-
-# perform write operation
-for match in writeableq:
-    to_csv(match, 'write_qual_pro.csv')
+for summonerName in players_to_follow:
+    write_summoner_to_csv(summonerName, has_headers)
+    has_headers = True
